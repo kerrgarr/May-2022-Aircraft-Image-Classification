7223acefd14f4d5f40c6c2f5c2f4b9eb40f8e76e
KEgarrett <105677187+KEgarrett@users.noreply.github.com>
2022-07-04 17:03:14 -0700

    Added CNN classifier network with more layers

--- code/models.py
+++ code/models.py
@@ -29,7 +29,7 @@ class LinearClassifier(torch.nn.Module):
     def forward(self, x):
         """
         @x: torch.Tensor((B,3,64,64))
-        @return: torch.Tensor((B,70))
+        @return: torch.Tensor((B,6))
         """
         return self.network(x.view(x.size(0), -1))
 
@@ -39,7 +39,7 @@ class MLPClassifier(torch.nn.Module):
         super().__init__()
 
         """
-        Your code here
+        Multilayer perceptron method
         """
         self.network = torch.nn.Sequential(
             torch.nn.Linear(3 * 64 * 64, 100),
@@ -56,7 +56,7 @@ class MLPClassifier(torch.nn.Module):
         """
         return self.network(x.view(x.size(0), -1))
 
-class CNNClassifier(torch.nn.Module):
+class CNNClassifier1(torch.nn.Module):
     def __init__(self, layers=[16, 32, 64, 128], n_input_channels=3, n_output_channels=6, kernel_size=5):
         super().__init__()
 
@@ -72,6 +72,44 @@ class CNNClassifier(torch.nn.Module):
     def forward(self, x):
         return self.classifier(self.network(x).mean(dim=[2, 3]))
 
+    
+class CNNClassifier(torch.nn.Module):
+    class Block(torch.nn.Module):
+        def __init__(self, n_input, n_output, kernel_size=3, stride=2):
+            super().__init__()
+            self.c1 = torch.nn.Conv2d(n_input, n_output, kernel_size=kernel_size, padding=kernel_size // 2,
+                                      stride=stride, bias=False)
+            self.c2 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2, bias=False)
+            self.c3 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2, bias=False)
+            self.b1 = torch.nn.BatchNorm2d(n_output)
+            self.b2 = torch.nn.BatchNorm2d(n_output)
+            self.b3 = torch.nn.BatchNorm2d(n_output)
+            self.skip = torch.nn.Conv2d(n_input, n_output, kernel_size=1, stride=stride)
+
+        def forward(self, x):
+            return F.relu(self.b3(self.c3(F.relu(self.b2(self.c2(F.relu(self.b1(self.c1(x)))))))) + self.skip(x))
+
+    def __init__(self, layers=[16, 32, 64, 128], n_output_channels=6, kernel_size=3):
+        super().__init__()
+        self.input_mean = torch.Tensor([0.3521554, 0.30068502, 0.28527516])
+        self.input_std = torch.Tensor([0.18182722, 0.18656468, 0.15938024])
+
+        L = []
+        c = 3
+        for l in layers:
+            L.append(self.Block(c, l, kernel_size, 2))
+            c = l
+        self.network = torch.nn.Sequential(*L)
+        self.classifier = torch.nn.Linear(c, n_output_channels)
+
+    def forward(self, x):
+        z = self.network((x - self.input_mean[None, :, None, None].to(x.device)) / self.input_std[None, :, None, None].to(x.device))
+        return self.classifier(z.mean(dim=[2, 3]))    
+    
+    
+    
+    
+    
 model_factory = {
     'linear': LinearClassifier,
     'mlp': MLPClassifier,
--- code/utils.py
+++ code/utils.py
@@ -4,24 +4,28 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 import pandas as pd
 import torchvision.transforms.functional as TR
+import torchvision.transforms as T
 
 #FAMILY_NAMES = ['A300','A310','A320','A330','A340','A380','ATR-42','ATR-72','An-12','BAE 146','BAE-125','Beechcraft 1900','Boeing 707','Boeing 717','Boeing 727','Boeing 737','Boeing 747','Boeing 757','Boeing 767','Boeing 777','C-130','C-47','CRJ-200','CRJ-700','Cessna 172','Cessna 208','Cessna Citation','Challenger 600','DC-10','DC-3','DC-6','DC-8','DC-9','DH-82','DHC-1','DHC-6','DR-400','Dash 8','Dornier 328','EMB-120','Embraer E-Jet','Embraer ERJ 145','Embraer Legacy 600','Eurofighter Typhoon','F-16','F/A-18','Falcon 2000','Falcon 900','Fokker 100','Fokker 50','Fokker 70','Global Express','Gulfstream','Hawk T1','Il-76','King Air','L-1011','MD-11','MD-80','MD-90','Metroliner','PA-28','SR-20','Saab 2000','Saab 340','Spitfire','Tornado','Tu-134','Tu-154','Yak-42']
 
-### Focus only on Airbus
 FAMILY_NAMES = ['A300','A310','A320','A330','A340','A380']
+
+mean_nums = [0.485, 0.456, 0.406]
+std_nums = [0.229, 0.224, 0.225]
 
 class AircraftDataset(Dataset):
     def __init__(self, dataset_path):
         """
-        Your code here
-        Hint: Use the python csv library to parse labels.csv
+        DataLoader
         """
         import csv
         from os import path
         #self.transform = transform
         self.data = []
-        to_tensor = transforms.ToTensor()
-        same_size = transforms.Resize((64, 64))
+        #to_tensor = transforms.ToTensor()
+        #same_size = transforms.Resize((256, 256))
+        #transform_image = T.Compose([T.Resize(size=256),T.CenterCrop(size=224),T.ToTensor(),T.Normalize(mean_nums, std_nums)])
+        transform_image = T.Compose([T.Resize(size=128),T.CenterCrop(size=112),T.ToTensor()])
         self.datapath = dataset_path
         with open(path.join(dataset_path, 'labels.csv'), newline='') as f:
             reader = csv.reader(f)
@@ -29,24 +33,20 @@ class AircraftDataset(Dataset):
                 if label in FAMILY_NAMES:
                     image = Image.open(path.join(dataset_path, fname + ".jpg"))
                     label_id = FAMILY_NAMES.index(label)
-                    tensor_image = to_tensor(image)
-                    image_resized = same_size(tensor_image)
+                    #tensor_image = to_tensor(image)
+                    #image_resized = same_size(tensor_image)
+                    image_resized = transform_image(image)
                     #image_resized = TR.resize(tensor_image, 64)
                     self.data.append((image_resized, label_id))
 
              
                     
     def __len__(self):
-        """
-        Your code here
-        """
+
         return len(self.data)
 
     def __getitem__(self, idx):
-        """
-        Your code here
-        return a tuple: img, label
-        """
+
         return self.data[idx]
     
 
