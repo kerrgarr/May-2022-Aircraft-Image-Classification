6cd8e78d3bb7b27318848450234e112c93d4bb11
KEgarrett <105677187+KEgarrett@users.noreply.github.com>
2022-07-06 09:36:18 -0700

    Update utils.py

--- code/utils.py
+++ code/utils.py
@@ -53,34 +53,3 @@ def accuracy(outputs, labels):
     return outputs_idx.eq(labels).float().mean()
 
 
-if __name__ == "__main__":
-    """
-    Measure how fast your data loader is.
-    """
-    from time import time
-    import argparse
-    parser = argparse.ArgumentParser("Loads the entire dataset once")
-    parser.add_argument('dataset')
-    parser.add_argument('-d', '--use_data_loader', action='store_true')
-    parser.add_argument('-p', '--plot', action='store_true')
-    parser.add_argument('-n', '--n_epoch', type=int, default=1)
-    args = parser.parse_args()
-
-    if args.use_data_loader:
-        L = load_data
-    else:
-        L = AircraftDataset
-
-    t0 = time()
-    data = L(args.dataset)
-
-    epoch_t = [time() - t0]
-    for i in range(args.n_epoch):
-        list(data)
-        epoch_t.append(time() - t0)
-    if args.plot:
-        import pylab as plt
-        plt.plot(epoch_t)
-        plt.show()
-    else:
-        print('Timing:', epoch_t)
