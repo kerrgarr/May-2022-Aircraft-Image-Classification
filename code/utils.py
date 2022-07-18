48531a97f14fb508392f0a676c8d7f3e593c6c33
KEgarrett <105677187+KEgarrett@users.noreply.github.com>
2022-07-17 19:54:24 -0700

    Update utils.py

--- code/utils.py
+++ code/utils.py
@@ -7,8 +7,8 @@ import torchvision.transforms.functional as TR
 import torchvision.transforms as T
 
 #FAMILY_NAMES = ['A300','A310','A320','A330','A340','A380','ATR-42','ATR-72','An-12','BAE 146','BAE-125','Beechcraft 1900','Boeing 707','Boeing 717','Boeing 727','Boeing 737','Boeing 747','Boeing 757','Boeing 767','Boeing 777','C-130','C-47','CRJ-200','CRJ-700','Cessna 172','Cessna 208','Cessna Citation','Challenger 600','DC-10','DC-3','DC-6','DC-8','DC-9','DH-82','DHC-1','DHC-6','DR-400','Dash 8','Dornier 328','EMB-120','Embraer E-Jet','Embraer ERJ 145','Embraer Legacy 600','Eurofighter Typhoon','F-16','F/A-18','Falcon 2000','Falcon 900','Fokker 100','Fokker 50','Fokker 70','Global Express','Gulfstream','Hawk T1','Il-76','King Air','L-1011','MD-11','MD-80','MD-90','Metroliner','PA-28','SR-20','Saab 2000','Saab 340','Spitfire','Tornado','Tu-134','Tu-154','Yak-42']
-
-FAMILY_NAMES = ['A300','A310','A320','A330','A340','A380']
+FAMILY_NAMES = ['Cessna 172','BAE-125','DR-400','Eurofighter Typhoon','Boeing 747','SR-20']
+#FAMILY_NAMES = ['A300','A310','A320','A330','A340','A380']
 
 mean_nums = [0.485, 0.456, 0.406]
 std_nums = [0.229, 0.224, 0.225]
@@ -21,7 +21,7 @@ class AircraftDataset(Dataset):
         import csv
         from os import path
         self.data = []
-        transform_image = T.Compose([T.Resize(size=128),T.CenterCrop(size=112),T.ToTensor()])
+        transform_image = T.Compose([T.Resize(size=128),T.ToTensor()])
         self.datapath = dataset_path
         with open(path.join(dataset_path, 'labels.csv'), newline='') as f:
             reader = csv.reader(f)
